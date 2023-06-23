a43d6452070185e49b1c2be706059e8d2078efc3
Jesus Guevara <jesusguevara23@gmail.com>
2023-06-23 19:28:54 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ["./src/**/*.jsx", "./src/**/*.js"],
+  darMode:false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
